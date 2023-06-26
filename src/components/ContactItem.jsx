864adb5fa9716aae9d327e5e32a455fc330f17ef
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-06-26 17:58:14 +0300

    add random color UKR

--- src/components/ContactItem.jsx
+++ src/components/ContactItem.jsx
@@ -57,12 +57,47 @@ export const ContactItem = ({ contact }) => {
       'y',
       'z',
     ];
+    const alphabetUkr = [
+      'а',
+      'б',
+      'в',
+      'г',
+      'ґ',
+      'д',
+      'е',
+      'є',
+      'ж',
+      'з',
+      'и',
+      'і',
+      'ї',
+      'й',
+      'к',
+      'л',
+      'м',
+      'н',
+      'о',
+      'п',
+      'р',
+      'с',
+      'т',
+      'у',
+      'ф',
+      'х',
+      'ц',
+      'ч',
+      'ш',
+      'щ',
+      'ь',
+      'ю',
+      'я',
+    ];
     const colors = [
       '#ff0000',
       '#ff4000',
       '#ff8000',
       '#ffcc00',
-      '#ffff00',
+      '#dddd00',
       '#ccff00',
       '#80ff00',
       '#40ff00',
@@ -70,7 +105,7 @@ export const ContactItem = ({ contact }) => {
       '#00ff40',
       '#00ff80',
       '#00ffcc',
-      '#00ffff',
+      '#00dddd',
       '#00ccff',
       '#00aaff',
       '#0080ff',
@@ -84,12 +119,26 @@ export const ContactItem = ({ contact }) => {
       '#ff00aa',
       '#ff0080',
       '#ff0040',
+      '#ff0000',
+      '#ff4000',
+      '#ff8000',
+      '#ffcc00',
+      '#dddd00',
+      '#ccff00',
+      '#80ff00',
     ];
     const userIndex = alphabet.findIndex(el => el === userLetter);
+    const userIndexUkr = alphabetUkr.findIndex(el => el === userLetter);
 
-    if (userIndex === -1) {
+    if (userIndex !== -1) {
+      return colors[userIndex];
+    }
+    if (userIndexUkr !== -1) {
+      return colors[userIndexUkr];
+    }
+    if (userIndex === -1 && userIndexUkr === -1) {
       return '#808080';
-    } else return colors[userIndex];
+    }
   };
 
   return (
